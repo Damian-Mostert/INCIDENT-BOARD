d619c93bec82ac4dca6edcef8940f53e09811e53
Damian <damian@fusebox.co.za>
2025-09-17 20:47:23 +0200

    fixed button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,7 +114,7 @@ export default function DoomsdayCounterPage() {
         <div className="flex items-center gap-4">
           <button
             onClick={handleReset}
-            className="px-4 py-2 text-white rounded-md border border-[#440000] bg-[#1a0101] hover:bg-[#2a0505] text-sm md:text-base uppercase"
+            className={`px-4 py-2 text-white rounded-md border border-[${getColor(days)}]  text-sm md:text-base uppercase`}
           >
             Reset
           </button>
